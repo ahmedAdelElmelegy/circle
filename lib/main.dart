cdfa565eff0061afd1309188fe9f2ff0550fab3e
Ahmed Adel <ahmedadelelmelegy2@gmail.com>
2025-05-17 23:20:02 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  // init
   init();
   runApp(
     EasyLocalization(
